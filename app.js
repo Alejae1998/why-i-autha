3f2a7c407d38e1cd3dcf4f951a45111a544c8ec9
Alejae1998 <alejagr1998@gmail.com>
2022-11-16 11:45:12 -0800

    signInForm done

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-import { signupUser } from './fetch-utils.js';
+import { signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
+// Wire up sign in and sign up forms to supabase
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
     const user = await signupUser(data.get('email'), data.get('password'));
 });
-// Wire up sign in and sign up forms to supabase
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
